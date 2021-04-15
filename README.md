8b459d1c7baab8b1f1948f46b2a1aba24221df07
scastrga <scastrga@everis.com>
2021-04-15 20:04:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-vgzrrlejercicio5-mjkwnx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vgzrrlejercicio5-mjkwnx)
---
+++ index.js
@@ -0,0 +1,19 @@
+// Crear un array de números del 1 al 10
+//  Mostrar por consola los números pares del array con el método for
+
+var arrayNum = [1,2,3,4,5,6,7,8,9,10];
+
+var pares = [];
+var impares = [];
+
+for(let i= 0;i<arrayNum.length;i++){
+    if(i % 2 ==0){
+        impares.push(array[i]);
+    }
+    else{
+        pares.push(array[i]);
+    }
+}
+
+console.log("Los numeros pares son " + pares);
+console.log("Los numeros impares son " + impares);
